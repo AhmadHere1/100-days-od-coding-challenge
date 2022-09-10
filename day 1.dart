0502daa06d7ab0dc15d5c451f6fb014cff11e219
AhmadHere <aghazi1011@gmail.com>
2022-09-09 23:09:23 -0700

    day 1

---
+++ day 1.dart
@@ -0,0 +1,15 @@
+//variables and data types
+
+String name = "Hello world. this is day 1 of my 100 days dart challenge.";
+int age = 25;
+bool isHeFit = true;
+double height = 23.4;
+double width = 23.456;
+int score = 234;
+void main() {
+  print(name);
+  print(isHeFit);
+  print(height);
+  print(width);
+  print(score);
+}
